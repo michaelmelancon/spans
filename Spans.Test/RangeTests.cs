19679d443fdd90608a3d510b9dbb645d4a67cd0d
Michael Melancon <michaelmelancon@gmail.com>
2017-03-01 10:21:02 -0600

    Updated project layout

--- Spans.Test/RangeTests.cs
+++ Spans.Test/RangeTests.cs
@@ -12,7 +12,7 @@ namespace Spans.Test
         [InlineData(1, 5, true, true, 3, 4, true, true)]
         [InlineData(1, 5, true, true, 3, 6, true, true)]
         [InlineData(1, 5, true, true, 0, 6, false, false)]
-        public void Overlaps(int sourceStart, int sourceEnd, bool sourceInclusiveStart, bool sourceInclusiveEnd, int targetStart, int targetEnd, bool targetInclusiveStart, bool targetInclusiveEnd)
+        public void OverlapsRange(int sourceStart, int sourceEnd, bool sourceInclusiveStart, bool sourceInclusiveEnd, int targetStart, int targetEnd, bool targetInclusiveStart, bool targetInclusiveEnd)
         {
             Assert.True(sourceStart.To(sourceEnd, sourceInclusiveStart, sourceInclusiveEnd).Overlaps(targetStart.To(targetEnd, targetInclusiveStart, targetInclusiveEnd)));
         }
@@ -21,7 +21,7 @@ namespace Spans.Test
         [InlineData(1, 5, true, false, 5, 10, true, true)]
         [InlineData(1, 5, true, true, 5, 10, false, true)]
         [InlineData(1, 5, true, true, 6, 10, true, true)]
-        public void DoesNotOverlap(int sourceStart, int sourceEnd, bool sourceInclusiveStart, bool sourceInclusiveEnd, int targetStart, int targetEnd, bool targetInclusiveStart, bool targetInclusiveEnd)
+        public void DoesNotOverlapRange(int sourceStart, int sourceEnd, bool sourceInclusiveStart, bool sourceInclusiveEnd, int targetStart, int targetEnd, bool targetInclusiveStart, bool targetInclusiveEnd)
         {
             Assert.False(sourceStart.To(sourceEnd, sourceInclusiveStart, sourceInclusiveEnd).Overlaps(targetStart.To(targetEnd, targetInclusiveStart, targetInclusiveEnd)));
         }
@@ -31,7 +31,7 @@ namespace Spans.Test
         [InlineData(1, 5, true, true, 1, 5, false, false)]
         [InlineData(1, 5, false, false, 1, 5, false, false)]
         [InlineData(1, 5, true, true, 2, 4, true, true)]
-        public void Includes(int sourceStart, int sourceEnd, bool sourceInclusiveStart, bool sourceInclusiveEnd, int targetStart, int targetEnd, bool targetInclusiveStart, bool targetInclusiveEnd)
+        public void IncludesRange(int sourceStart, int sourceEnd, bool sourceInclusiveStart, bool sourceInclusiveEnd, int targetStart, int targetEnd, bool targetInclusiveStart, bool targetInclusiveEnd)
         {
             Assert.True(sourceStart.To(sourceEnd, sourceInclusiveStart, sourceInclusiveEnd).Includes(targetStart.To(targetEnd, targetInclusiveStart, targetInclusiveEnd)));
         }
@@ -41,7 +41,7 @@ namespace Spans.Test
         [InlineData(1, 5, true, true, 3)]
         [InlineData(1, 5, false, false, 3)]
         [InlineData(1, 5, true, true, 5)]
-        public void Includes(int sourceStart, int sourceEnd, bool sourceInclusiveStart, bool sourceInclusiveEnd, int target)
+        public void IncludesValue(int sourceStart, int sourceEnd, bool sourceInclusiveStart, bool sourceInclusiveEnd, int target)
         {
             Assert.True(sourceStart.To(sourceEnd, sourceInclusiveStart, sourceInclusiveEnd).Includes(target));
         }      
@@ -50,7 +50,7 @@ namespace Spans.Test
         [InlineData(1, 5, false, false, 1, 5, true, true)]
         [InlineData(1, 5, true, true, 1, 6, true, true)]
         [InlineData(1, 5, true, true, 0, 5, true, true)]
-        public void DoesNotInclude(int sourceStart, int sourceEnd, bool sourceInclusiveStart, bool sourceInclusiveEnd, int targetStart, int targetEnd, bool targetInclusiveStart, bool targetInclusiveEnd)
+        public void DoesNotIncludeRange(int sourceStart, int sourceEnd, bool sourceInclusiveStart, bool sourceInclusiveEnd, int targetStart, int targetEnd, bool targetInclusiveStart, bool targetInclusiveEnd)
         {
             Assert.False(sourceStart.To(sourceEnd, sourceInclusiveStart, sourceInclusiveEnd).Includes(targetStart.To(targetEnd, targetInclusiveStart, targetInclusiveEnd)));
         }
@@ -60,7 +60,7 @@ namespace Spans.Test
         [InlineData(1, 5, false, false, 1)]
         [InlineData(1, 5, false, false, 5)]
         [InlineData(1, 5, false, false, 6)]
-        public void DoesNotInclude(int sourceStart, int sourceEnd, bool sourceInclusiveStart, bool sourceInclusiveEnd, int target)
+        public void DoesNotIncludeValue(int sourceStart, int sourceEnd, bool sourceInclusiveStart, bool sourceInclusiveEnd, int target)
         {
             Assert.False(sourceStart.To(sourceEnd, sourceInclusiveStart, sourceInclusiveEnd).Includes(target));
         }  
